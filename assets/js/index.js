f9a1ef323926a72f2a51ce9c50535e54cc0cfdda
val8119 <mikk.vahaste@gmail.com>
2020-06-26 00:27:55 +0300

    Added clear button, fixed line breaks

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,6 @@
 const notepadTextarea = document.querySelector(".notepad-textarea");
 const saveButton = document.querySelector(".save-button");
+const clearButton = document.querySelector(".clear-button");
 
 // set text on load
 window.addEventListener("load", function () {
@@ -9,4 +10,9 @@ window.addEventListener("load", function () {
 // save text
 saveButton.addEventListener("click", function () {
     localStorage.setItem("notepadText", notepadTextarea.value);
+});
+
+// save text
+clearButton.addEventListener("click", function () {
+    notepadTextarea.value = "";
 });
